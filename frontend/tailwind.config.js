c652091cf3a54e37f2c2a96a3500805fe344c109
Flacco <lgalvez1998@yahoo.com>
2025-12-19 20:06:15 -0800

    Downgrade to Tailwind v3 for stable deployment

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '25': '6.25rem',
+        '30': '7.5rem',
+        '35': '8.75rem',
+      },
+    },
+  },
+  plugins: [],
+}
