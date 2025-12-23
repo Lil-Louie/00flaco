b1c6cd5961c15efb628956b6be83f06b543cba6f
Flacco <lgalvez1998@yahoo.com>
2025-12-22 16:36:10 -0800

    Games: changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Games from './pages/Games';
-import Snake from './games/snake/snake';   // example
-import Wordle from './games/wordle/Wordle';     // when you have it
+import Snake from './games/snake/snake';
+import Wordle from './games/wordle/Wordle';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,8 +11,24 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<Games />} />
-        <Route path="/games/snake" element={<Snake />} />
-        <Route path="/games/wordle" element={<Wordle />} />
+
+        <Route
+          path="/games/snake"
+          element={
+            <div className="h-[100dvh] overflow-hidden overscroll-none">
+              <Snake />
+            </div>
+          }
+        />
+
+        <Route
+          path="/games/wordle"
+          element={
+            <div className="h-[100dvh] overflow-hidden overscroll-none">
+              <Wordle />
+            </div>
+          }
+        />
       </Routes>
     </Layout>
   );
